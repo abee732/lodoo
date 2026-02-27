aa6fc364729300992d11bff2534f60e0528f9f3f
MAC <admin@MacBook-Pro.local>
2026-02-27 17:20:33 +0700

    update code

--- addons/odoo_ai_assistant/models/odoo_ai_chat.py
+++ addons/odoo_ai_assistant/models/odoo_ai_chat.py
@@ -14,7 +14,7 @@ class OdooAIChat(models.Model):
         string='Người gửi', 
         default=lambda self: self.env.user
     )
-    message = fields.Text(string="Question")
+    question = fields.Text(string="Question")
     answer = fields.Text(string="Answer", readonly=True) # Nên để readonly để tránh sửa tay đè lên AI
 
     def action_send(self):
